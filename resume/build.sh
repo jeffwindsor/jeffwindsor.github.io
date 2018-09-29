c21920cd5d34e23ebaf377b5f2ddbf6688586418
Jeff Windsor <jeff.windsor@gmail.com>
2018-09-29 13:54:52 -0700

    Remove ignored files

---
+++ resume/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+resume export resume
+resume export resume-elegant.html -t elegant
+resume export resume-short.html -t short
+resume export resume-stackoverflow.html -t stackoverflow
+resume export resume-spartan.html -t spartan
+resume export resume-classy.html -t classy
