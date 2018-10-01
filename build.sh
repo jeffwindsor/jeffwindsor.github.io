ea507555bee14ab834929eea314171e96b579cba
Jeff Windsor <jeff.windsor@gmail.com>
2018-09-30 21:29:45 -0700

    swiss theme

--- build.sh
+++ build.sh
@@ -2,7 +2,3 @@
 cd ./resume
 resume export resume.html -t stackoverflow
 resume export resume2.html -t elegant
-#resume export resume-short.html -t short
-#resume export resume-stackoverflow.html -t stackoverflow
-#resume export resume-spartan.html -t spartan
-#resume export resume-classy.html -t classy
