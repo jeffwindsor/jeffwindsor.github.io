bdc3092b5de70f5e3c55b0d8ece176afd6f5a218
Jeff Windsor <jeff.windsor@gmail.com>
2018-10-03 21:13:34 -0700

    resumes

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 cd ./resume
-resume export resume.html -t stackoverflow
-resume export resume2.html -t elegant
+
+templates=(
+  elegant
+  paper
+  kendall
+  flat
+  modern
+  classy
+  class
+  short
+  slick
+  kwan
+  onepage
+  spartan
+  stackoverflow
+)
+for t in "${templates[@]}"; do
+  npm install -g jsonresume-theme-${t}
+  resume export resume-${t}.html -t ${t}
+done
+
