c8e2911e66fddb1ba8f2926b99af36482f42392d
Jeff Windsor <jeff.windsor@gmail.com>
2018-10-03 23:55:43 -0700

    resumes

--- build.sh
+++ build.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-resume_list=resumes.html
-formats=(doc html md txt pdf)
-json=(material cora elegant paper kendall flat modern classy class short slick onepage spartan stackoverflow)
-fresh=(modern compact awesome /usr/local/lib/node_modules/fresh-theme-positive /usr/local/lib/node_modules/fresh-theme-underscore)
+resume_list=resumes.md
+formats=(html pdf)
+json=(elegant kendall flat class short spartan stackoverflow)
+fresh=(modern compact)
 
-cd ./resume
-
-echo "<html><head></head><body><h1>Generated Resume Formats</h1><ul>" > ${resume_list}
+echo "# Jeff Windsor's Resumes" > ${resume_list}
+echo ""  >> ${resume_list}
+echo "[Home](https://jeffwindsor.carrd.co/) > Resumes"
+echo ""  >> ${resume_list}
+echo "## Resume by Format" >> ${resume_list}
+echo ""  >> ${resume_list}
 
 ## pre convert json-resume format to fresh format 
-hackmyresume convert resume.json TO resume-fresh.json
+hackmyresume convert ./resume/resume.json TO ./resume/resume-fresh.json
 
 ## resume.json is data of record
 for t in "${json[@]}"; do
     echo ${t}
     npm i -g jsonresume-theme-${t}
-    echo "<li>${t} <a href="generated/resume-json-${t}.html">[ html ]</a> <a href="generated/resume-json-${t}.pdf">[ pdf ]</a> <a href="generated/resume-json-${t}.txt">[ txt ]</a> <a href="generated/resume-json-${t}.md">[ md ]</a> </li>" >> ${resume_list}
+    echo "  * ${t} [[ html ](generated/resume-json-${t}.html)] [[ pdf ](generated/resume-json-${t}.pdf)]" >> ${resume_list}
     for f in "${formats[@]}"; do
-      hackmyresume build resume.json TO generated/resume-json-${t}.${f} -t /usr/local/lib/node_modules/jsonresume-theme-${t}
+      hackmyresume build ./resume/resume.json TO ./resume/generated/resume-json-${t}.${f} -t /usr/local/lib/node_modules/jsonresume-theme-${t}
     done
 done
 
@@ -25,10 +28,8 @@ done
 for t in "${fresh[@]}"; do
   echo ${t}
   npm i -g fresh-theme-${t}
-  echo "<li>${t} <a href="generated/resume-fresh-${t}.html">[ html ]</a> <a href="generated/resume-fresh-${t}.pdf">[ pdf ]</a> <a href="generated/resume-fresh-${t}.txt">[ txt ]</a> <a href="generated/resume-fresh-${t}.md">[ md ]</a></li>" >> ${resume_list}
+  echo "  * ${t} [[ html ](generated/resume-fresh-${t}.html)] [[ pdf ](generated/resume-fresh-${t}.pdf)]" >> ${resume_list}
   for f in "${formats[@]}"; do
-    hackmyresume build resume-fresh.json TO generated/resume-fresh-${t}.${f} -t ${t}
+    hackmyresume build ./resume/resume-fresh.json TO ./resume/generated/resume-fresh-${t}.${f} -t ${t}
   done
 done
-
-echo "</ul></body></html>" >> resumes.html
---
+++ resumes.md
@@ -0,0 +1,14 @@
+# Jeff Windsor's Resumes
+
+
+## Resume by Format
+
+  * elegant [[ html ](generated/resume-json-elegant.html)] [[ pdf ](generated/resume-json-elegant.pdf)]
+  * kendall [[ html ](generated/resume-json-kendall.html)] [[ pdf ](generated/resume-json-kendall.pdf)]
+  * flat [[ html ](generated/resume-json-flat.html)] [[ pdf ](generated/resume-json-flat.pdf)]
+  * class [[ html ](generated/resume-json-class.html)] [[ pdf ](generated/resume-json-class.pdf)]
+  * short [[ html ](generated/resume-json-short.html)] [[ pdf ](generated/resume-json-short.pdf)]
+  * spartan [[ html ](generated/resume-json-spartan.html)] [[ pdf ](generated/resume-json-spartan.pdf)]
+  * stackoverflow [[ html ](generated/resume-json-stackoverflow.html)] [[ pdf ](generated/resume-json-stackoverflow.pdf)]
+  * modern [[ html ](generated/resume-fresh-modern.html)] [[ pdf ](generated/resume-fresh-modern.pdf)]
+  * compact [[ html ](generated/resume-fresh-compact.html)] [[ pdf ](generated/resume-fresh-compact.pdf)]
